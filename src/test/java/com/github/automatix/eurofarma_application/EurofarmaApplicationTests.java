2e768e8558ed9077440184261e28bdc8fc8f89f0
vitorzottino <v.zottino@reply.com>
2024-09-18 19:47:20 -0300

    add base project

---
+++ src/test/java/com/github/automatix/eurofarma_application/EurofarmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.automatix.eurofarma_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurofarmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
